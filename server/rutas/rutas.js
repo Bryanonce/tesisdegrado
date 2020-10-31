00aebf333f6c37a3321f634b39e854200cd66575
Bryan Cedeño <bryan.cedeo@gmail.com>
2020-10-31 12:54:00 -0500

    mejorar la bajada de recursos de la api

--- server/rutas/rutas.js
+++ server/rutas/rutas.js
@@ -3,6 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const baseDatos = require('../models/datos');
 // parse application/x-www-form-urlencoded
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
